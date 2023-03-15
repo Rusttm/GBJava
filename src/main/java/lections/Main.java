06429bca5e698d9aca1c379a92303474eacbf461
Rustam <52700794+Rusttm@users.noreply.github.com>
2023-03-15 20:43:47 +0300

    lesson2 initial commit

--- src/main/java/lections/Main.java
+++ src/main/java/lections/Main.java
@@ -1,7 +1,14 @@
 package lections;
 
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Lections");
+        workWithFile();
+    }
+    public static void workWithFile() {
+        System.out.println("Lections");
+        File f1 = new File("data.txt");
+        System.out.println(f1.getAbsolutePath());
     }
 }
