05c0d7a20ad0d4e4680bd073816334b51f5c6cb7
Rustam <52700794+Rusttm@users.noreply.github.com>
2023-03-15 13:10:50 +0300

    lesson1 final commit

--- src/main/java/lesson1/Main.java
+++ src/main/java/lesson1/Main.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Привет");
+
         // 1. Задать одномерный массив и найти в нем минимальный и максимальный элементы
         System.out.printf("Максимальный элемент =%d", findMinMax());
         System.out.println();
@@ -14,6 +15,17 @@ public class Main {
         // не високосный - false). Каждый 4-й год является високосным, кроме каждого 100-го,
         // при этом каждый 400-й – високосный.
         System.out.printf("Год высокосный %b", checkYear());
+        System.out.println();
+
+//        3. Дан массив nums = [3,2,2,3] и число val = 3.
+//        Если в массиве есть числа, равные заданному, нужно перенести эти элементы в конец массива.
+//        Таким образом, первые несколько (или все) элементов массива должны быть отличны от заданного,
+//        а остальные - равны ему.
+        int[] myshiftedarray = shiftArray();
+        System.out.print("Смещенный массив:");
+        System.out.println(Arrays.toString(myshiftedarray));
+
+
         }
     private static int findMinMax() {
         int[] myarray = {3,5,7,9,5,1,3,2};
@@ -30,7 +42,6 @@ public class Main {
 
     }
     private static boolean checkYear() {
-
         Scanner input =  new Scanner(System.in);
         System.out.print("Enter year: ");
         String year = input.next();
@@ -42,5 +53,26 @@ public class Main {
             return false;
         }
         else return (yearint % 4) == 0;
+    }
+    private static int[] shiftArray() {
+        int[] myarray = {3,2,1,3,2,5,7,3};
+        System.out.print("Задан массив:");
+        System.out.println(Arrays.toString(myarray));
+        Scanner in =  new Scanner(System.in);
+        System.out.print("Введите число: ");
+        String inputnum = in.next();
+        int inputint = Integer.parseInt(String.valueOf(inputnum));
+        int right = myarray.length;
+        for (int i=0; i < right; i++) {
+            for (int j=right-1; j > i; j--){
+                if ((myarray[i] == inputint) & (myarray[j] != inputint)) {
+                    myarray[i] = myarray[j];
+                    myarray[j] = inputint;
+//                    System.out.println(Arrays.toString(myarray));
+                }
+            }
+        }
+
+        return  myarray;
     }
 }
