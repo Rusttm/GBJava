db9a6d226f2025d75631ac57770a68cf5ed9cab0
Rustam <52700794+Rusttm@users.noreply.github.com>
2023-03-22 21:19:44 +0300

    lesson4  commit

--- src/main/java/lesson4/Main.java
+++ src/main/java/lesson4/Main.java
@@ -1,9 +1,29 @@
 package lesson4;
 
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("lesson4");
+//        Задача 1
+//        1. Реализовать консольное приложение, которое:
+//        Принимает от пользователя и “запоминает” строки.
+//        Если введено print, выводит строки так, чтобы последняя введенная была первой в списке,
+//        а первая - последней.
+//        Если введено revert, удаляет предыдущую введенную строку из памяти.
+        FormatList();
 
+
+    }
+
+    public static void FormatList() {
+
+        while (true) {
+            System.out.print("Введите строку: ");
+            Scanner input =  new Scanner(System.in);
+            String inputString = input.next();
+            System.out.println(inputString);
+            break;
+        }
     }
 
 }
