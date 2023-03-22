7eae5c8499ae8870341b2e5f27d13ecb1d7c4696
Rustam <52700794+Rusttm@users.noreply.github.com>
2023-03-22 17:09:20 +0300

    lesson2 commit

--- src/main/java/lesson2/Main.java
+++ src/main/java/lesson2/Main.java
@@ -3,5 +3,16 @@ package lesson2;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Lesson 2");
+
     }
+    public static boolean Palindrom(String st) {
+        System.out.println("Введите строку");
+        return true;
+
+    }
+
+//            1. Напишите метод, который принимает на вход строку (String) и
+//        определяет является ли строка палиндромом (возвращает boolean значение).
+
+
 }
