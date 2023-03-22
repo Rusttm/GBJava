c4a41206ec8ce5133b932141a3e07276146793f9
Rustam <52700794+Rusttm@users.noreply.github.com>
2023-03-22 17:17:06 +0300

    lesson2 commit

--- src/main/java/lesson2/Main.java
+++ src/main/java/lesson2/Main.java
@@ -1,8 +1,13 @@
 package lesson2;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Lesson 2");
+        Scanner input =  new Scanner(System.in);
+        System.out.print("Введите строку: ");
+        String paliString = input.next();
 
     }
     public static boolean Palindrom(String st) {
