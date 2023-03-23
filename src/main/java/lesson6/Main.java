bc3b5612214e7523b8fcdaf47345490752ac38ab
Rustam <52700794+Rusttm@users.noreply.github.com>
2023-03-24 01:21:38 +0300

    lesson6 fin commit

--- src/main/java/lesson6/Main.java
+++ src/main/java/lesson6/Main.java
@@ -1,8 +1,54 @@
 package lesson6;
 
 
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Scanner;
+
+import static java.lang.System.exit;
+
 public class Main {
     public static void main(String[] args) {
+        ArrayList <Notebooks> notebookBase = new ArrayList<>();
+
+        Notebooks notebook = new Notebooks("ZenBook", "Lenovo", "black", "8");
+        notebookBase.add(notebook);
+        notebookBase.add(new Notebooks("ZenBook", "Lenovo", "black", "16"));
+        notebookBase.add(new Notebooks("Office", "Dell", "grey", "8"));
+        notebookBase.add(new Notebooks("Office", "Dell", "grey", "4"));
+        notebookBase.add(new Notebooks("Macbook M1", "Apple", "space grey", "4"));
+        notebookBase.add(new Notebooks("Macbook M2", "Apple", "space grey", "16"));
+        notebookBase.add(new Notebooks("Macbook M2", "Apple", "space grey", "16"));
+        notebookBase.add(new Notebooks("Laptop", "Asus", "black", "8"));
+        notebookBase.add(new Notebooks("Extensa", "Acer", "black", "16"));
+        notebookBase.add(new Notebooks("Aspier", "Acer", "black", "4"));
+        notebookBase.add(new Notebooks("IdeaPad", "Lenovo", "gray", "8"));
+        FilterNotebooks(notebookBase);
+    }
+    public static void FilterNotebooks(ArrayList <Notebooks> notebookBase) {
+
+
+        // получим имеющиеся поля из любого экзэмпляра
+        HashSet<String> filtersFields = notebookBase.get(0).GetFilters();
+//        System.out.println(filtersFields);
+        System.out.print("Задание3. Введите название фильтра (" + filtersFields + "): ");
+        Scanner input =  new Scanner(System.in);
+        String filterKey = input.next();
+        if (!filtersFields.contains(filterKey)) {
+            System.out.println("Такого фильтра нет");
+            exit(1);
+        }
+        System.out.print("Введите значение фильтра: ");
+        String filterValue = input.next();
+
+        for (Notebooks notebook:
+             notebookBase) {
+            if (notebook.IsApplyFilter(filterKey, filterValue)) {
+                System.out.println(notebook);
+            }
+
+        }
 
     }
 
---
+++ src/main/java/lesson6/Notebooks.java
@@ -0,0 +1,106 @@
+package lesson6;
+
+import java.util.HashSet;
+import java.util.Objects;
+
+public class Notebooks {
+
+    private String name;
+    private String brand;
+    private String color;
+    private double weight = 2.45;//deprecated
+    private double diag = 14.5; //deprecated
+    private String memory;
+    private int stock = 0;//deprecated
+
+
+    public Notebooks(String noteName, String noteBrand, String noteColor, String noteMemory) {
+        this.name = noteName;
+        this.brand = noteBrand;
+        this.color = noteColor;
+        this.memory = noteMemory;
+        System.out.println("Создан ноутбук " + this.name  + this.brand);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getMemory() {
+        return memory;
+    }
+
+    public void setMemory(String memory) {
+        this.memory = memory;
+    }
+
+    public HashSet<String> GetFilters() {
+        HashSet<String> propKeys = new HashSet<>();
+        propKeys.add("name");
+        propKeys.add("brand");
+        propKeys.add("color");
+        propKeys.add("memory");
+        return propKeys;
+    }
+
+    public boolean IsApplyFilter(String filterKey, String filterValue) {
+//        System.out.println(filterKey + filterValue);
+        if (Objects.equals(filterKey, "name")) {
+
+            return Objects.equals(filterValue, this.name);
+        } else if (Objects.equals(filterKey, "brand")) {
+            return Objects.equals(filterValue, this.brand);
+        } else if (Objects.equals(filterKey, "color")) {
+
+            return Objects.equals(filterValue, this.color);
+        } else if (Objects.equals(filterKey, "memory")) {
+            return Objects.equals(filterValue, this.memory);
+        } else {
+            return false;
+        }
+
+    }
+
+    @Override
+    public String toString() {
+        return "Notebooks{" +
+                "name='" + name + '\'' +
+                ", brand='" + brand + '\'' +
+                ", color='" + color + '\'' +
+                ", weight=" + weight +
+                ", diag=" + diag +
+                ", memory='" + memory + '\'' +
+                ", stock=" + stock +
+                '}';
+    }
+
+    public String GetInfo() {
+        return name;
+    }
+
+    public void BuyNote() {
+        stock += 1;
+    }
+
+    public void SellNote() {
+        if (stock>0) {
+            stock -= 1;
+        }
+        else {
+            System.out.println("Нет на складе!");
+        }
+
+    }
+}
