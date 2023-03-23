a74619edb26f1e6a105ae89afd097d80d48942a4
Rustam <52700794+Rusttm@users.noreply.github.com>
2023-03-23 15:41:08 +0300

    lesson5 fin commit

--- src/main/java/lesson5/Main.java
+++ src/main/java/lesson5/Main.java
@@ -1,27 +1,30 @@
 package lesson5;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("lesson5");
         //        Задача 1
-        System.out.println("Задача 1");
+        System.out.print("Задача 1. ");
 //        1. Реализуйте структуру телефонной книги с помощью HashMap, учитывая,
 //        что 1 человек может иметь несколько телефонов.
         HashMap<String, List<String>> phoneBook = new HashMap<>();
-        ArrayList<String> l = new ArrayList<>();
-        l.add("71234564567890");
-        l.add("31276645378986");
-        phoneBook.put("Nata", l);
+        phoneBook.computeIfAbsent("Natalia Ivanova", k -> new ArrayList<>()).add("31276645355511");
+        phoneBook.computeIfAbsent("Natalia Ivanova", k -> new ArrayList<>()).add("71234564567890");
+        phoneBook.computeIfAbsent("Dmitri Petroff", k -> new ArrayList<>()).add("81233553223332");
+        phoneBook.computeIfAbsent("Dmitri Petroff", k -> new ArrayList<>()).add("51276645367889");
+        phoneBook.computeIfAbsent("Dmitri Petroff", k -> new ArrayList<>()).add("41200900505054");
+
+
+        System.out.println("Телефонная книга, в которой у сотрудника несколько телефонов: ");
         System.out.println(phoneBook);
 
 
         //        Задача 2
-        System.out.println("Задача 2");
+        System.out.println("Задача 2.");
 //        Пусть дан список сотрудников:
 //        Написать программу, которая найдёт и
 //        выведет повторяющиеся имена с количеством повторений.
@@ -58,13 +61,40 @@ public class Main {
             }
 
         }
-
-        System.out.println(SortHashValue(namesBook));
+        System.out.print("Начальный словарь: ");
+        System.out.println(namesBook);
+        System.out.print("Отсортированный словарь: ");
+        System.out.println(SortHashByValue(namesBook));
 
 
    }
-    public static HashMap<String, Integer> SortHashValue(HashMap<String, Integer> myHashMap) {
+    public static HashMap<String, Integer> SortHashByValue(HashMap<String, Integer> myHashMap) {
         // написать функцию сортировки
-        return myHashMap;
+        List<String> mapKeys = new ArrayList<>(myHashMap.keySet());
+        List<Integer> mapValues = new ArrayList<>(myHashMap.values());
+        Collections.sort(mapValues);
+        Collections.reverse(mapValues); // descending
+        Collections.sort(mapKeys);
+//        System.out.println(mapValues);
+        LinkedHashMap<String, Integer> sortedMap = new LinkedHashMap<>();
+        // делаем через итераторы
+        Iterator<Integer> valueIt = mapValues.iterator();
+        while (valueIt.hasNext()) {
+            Integer val = valueIt.next();
+            Iterator<String> keyIt = mapKeys.iterator();
+            while (keyIt.hasNext()) {
+                String key = keyIt.next();
+                Integer val1 = myHashMap.get(key);
+                Integer val2 = val;
+                if (val1.equals(val2)) {
+                    keyIt.remove();
+                    sortedMap.put(key, val);
+                    break;
+                }
+            }
+
+        }
+
+        return sortedMap;
     }
 }
